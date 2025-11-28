6a65f3dfc8d438cb14625b3af9e0ab2dccf72aa9
kolenka-matka <zlatkina.ekaterina@gmail.com>
2025-11-28 16:35:17 +0300

    3st commit for 3d task

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main{
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Коммит 1 в dev");
         System.out.println("Коммит 2 в dev");
+        System.out.println("Коммит 3 в dev");
     }
 }
